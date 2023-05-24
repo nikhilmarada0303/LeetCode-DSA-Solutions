bb0b9bb8722bdd1554aaab39b5fb0ba1382781a6
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-05-24 23:18:50 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -6,34 +6,22 @@ using namespace std;
 class Solution {
   public:
     
-    long long int func(int ind,int coins[],int target,vector<vector<long>>&dp){
-        if(ind==0){
-           return (target%coins[ind]==0);
-        }
-        if(dp[ind][target]!=-1) return dp[ind][target];
-        long long notTake=func(ind-1,coins,target,dp);
-        long long take=0;
-        if(coins[ind]<=target)
-        take=func(ind,coins,target-coins[ind],dp);
-        return dp[ind][target]=take+notTake;
-        
-    }
     long long int count(int coins[], int n, int sum) {
-        vector<vector<long>>dp(n,vector<long>(sum+1,0));
+       vector<long>prev(sum+1,0),curr(sum+1,0);
        for(int t=0;t<=sum;t++){
-           dp[0][t]=(t%coins[0]==0);
+           prev[t]=(t%coins[0]==0);
        }
         for(int ind=1;ind<n;ind++){
             for(int target=0;target<=sum;target++){
-                long long notTake=dp[ind-1][target];
+                long long notTake=prev[target];
                 long long take=0;
                 if(coins[ind]<=target)
-                take=dp[ind][target-coins[ind]];
-                dp[ind][target]=take+notTake;
+                take=curr[target-coins[ind]];
+                curr[target]=take+notTake;
         
-            }
+            }prev=curr;
         }
-        return dp[n-1][sum];
+        return prev[sum];
     }
 };
 
