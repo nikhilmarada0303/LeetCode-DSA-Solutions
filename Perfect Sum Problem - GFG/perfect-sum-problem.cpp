8fa682cbdf13c0164c55375f97f99528073aa753
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-05-24 15:46:17 +0530

    Added solution - LeetHub

---
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+	public:
+int mod=(int)(1e9+7);
+int func(int ind,int target,int arr[],vector<vector<int>>&dp){
+	    
+	    if(ind==0) {
+	        if(target==0 && arr[ind]==0) return 2;
+	        if(target==0 || target==arr[0]) return 1;
+	        else
+	        return 0;
+	    }
+	    if(dp[ind][target]!=-1) return dp[ind][target];
+	    int take=0;
+	    if(arr[ind]<=target)
+	    take=func(ind-1,target-arr[ind],arr,dp);
+	    int notTake=func(ind-1,target,arr,dp);
+	    return dp[ind][target]=(take + notTake)%mod;
+	}
+	int perfectSum(int arr[], int n, int sum)
+	{
+	    vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        return func(n-1,sum,arr,dp);
+	}
+
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
