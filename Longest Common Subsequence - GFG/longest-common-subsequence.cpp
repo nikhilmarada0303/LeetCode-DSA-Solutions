73bc2be568eecf329b05badc42d8b23f03ccd609
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-05-27 12:03:34 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -9,33 +9,19 @@ using namespace std;
 class Solution
 {
     public:
-    int func(int ind1,int ind2,string &s1,string &s2,vector<vector<int>>&dp){
-        if(ind1<0 || ind2<0) return 0;
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        int match=INT_MIN;
-        
-        if(s1[ind1]==s2[ind2])
-            match= 1+func(ind1-1,ind2-1,s1,s2,dp);
-        
-        int not_match= 0+max(func(ind1-1,ind2,s1,s2,dp),func(ind1,ind2-1,s1,s2,dp));
-        return dp[ind1][ind2]=max(match,not_match);
-    }
     int lcs(int x, int y, string s1, string s2)
     {
-        vector<vector<int>>dp(x+1,vector<int>(y+1,0));
-        for(int i=0;i<=x;i++)
-            dp[i][0]=0;
+       vector<int>prev(y+1,0),curr(y+1,0);
         for(int i=0;i<=y;i++)
-            dp[0][i]=0;
+            prev[i]=0;
         for(int ind1=1;ind1<=x;ind1++){
             for(int ind2=1;ind2<=y;ind2++){
                  if(s1[ind1-1]==s2[ind2-1])
-                    dp[ind1][ind2]= 1+dp[ind1-1][ind2-1];
+                    curr[ind2]= 1+prev[ind2-1];
                 else{
-                dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1]);}
-                 
-            }
-        }return dp[x][y];
+                curr[ind2]=max(prev[ind2],curr[ind2-1]);}
+            }prev=curr;
+        }return prev[y];
       
     }
 };
