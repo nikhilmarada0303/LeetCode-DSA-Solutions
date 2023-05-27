8a698dcd2b142662447e9877a7927d71756d4f94
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-05-27 22:52:21 +0530

    Added solution - LeetHub

---
+++ Form a palindrome - GFG/form-a-palindrome.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+     int lcs(int x, int y, string s1, string s2)
+    {
+        vector<vector<int>>dp(x+1,vector<int>(y+1,0));
+        for(int i=0;i<=x;i++)
+            dp[i][0]=0;
+        for(int i=0;i<=y;i++)
+            dp[0][i]=0;
+        for(int ind1=1;ind1<=x;ind1++){
+            for(int ind2=1;ind2<=y;ind2++){                                         
+                 if(s1[ind1-1]==s2[ind2-1])
+                    dp[ind1][ind2]= 1+dp[ind1-1][ind2-1];
+                else{
+                dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1]);}
+                 
+            }
+        }return dp[x][y];
+      
+    }
+    int longestPalinSubseq(string s1) {
+        string s2=s1;
+        reverse(s2.begin(),s2.end());
+        return lcs(s1.size(),s1.size(),s1,s2);
+    }
+    
+    int countMin(string s1){
+        return s1.size()-longestPalinSubseq(s1);
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.countMin(str) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
