b309f122c4634fbd19cce7bfc06e1ee2dd2ec990
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-05-29 16:59:19 +0530

    Added solution - LeetHub

--- Maximum Profit - GFG/maximum-profit.cpp
+++ Maximum Profit - GFG/maximum-profit.cpp
@@ -9,32 +9,21 @@ using namespace std;
 
 class Solution {
   public:
-  int func(int ind,int transaction,int price[],int n,int k,vector<vector<int>>&dp){
-    if(transaction==2*k) return 0;
-    if(ind==n) return 0;
-    int profit=0;
-    if(dp[ind][transaction]!=-1) return dp[ind][transaction];
-    if(transaction%2==0){
-        profit=max(-price[ind]+func(ind+1,transaction+1,price,n,k,dp),func(ind+1,transaction,price,n,k,dp));
-    }else
-        profit=max(price[ind]+func(ind+1,transaction+1,price,n,k,dp),func(ind+1,transaction,price,n,k,dp));
-        return dp[ind][transaction]=profit;
-}
 
     int maxProfit(int k, int n, int price[]) {
-        vector<vector<int>>dp(n+1,vector<int>(2*k+1,0));
+        vector<int>curr(2*k+1,0);
+        vector<int>ahead(2*k+1,0);
         // base cases are inititated to zero
         for(int ind=n-1;ind>=0;ind--){
             for(int transaction=(2*k)-1;transaction>=0;transaction--){
                 int profit=0;
                 if(transaction%2==0)
-                profit=max(-price[ind]+dp[ind+1][transaction+1],dp[ind+1][transaction]);
+                profit=max(-price[ind]+ahead[transaction+1],ahead[transaction]);
                 else
-                  profit=max(price[ind]+dp[ind+1][transaction+1],dp[ind+1][transaction]);
-                  dp[ind][transaction]=profit;
-            }
-        }return dp[0][0];
-    
+                  profit=max(price[ind]+ahead[transaction+1],ahead[transaction]);
+                  curr[transaction]=profit;
+            }ahead=curr;
+        }return ahead[0];
     }
 };
 
