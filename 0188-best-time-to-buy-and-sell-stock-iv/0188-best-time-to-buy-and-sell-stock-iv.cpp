82b781734ec27e4d36a3d3e79e536c0e8a50ceac
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-05-29 17:01:22 +0530

    Time: 11 ms (86.98%), Space: 10.8 MB (98.21%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& price) {
         int n=price.size();
-         vector<vector<int>>after(2,vector<int>(k+1,0));
-     vector<vector<int>>curr(2,vector<int>(k+1,0));
-     
-    // base cases are already zeros
-        //when cnt==0 ind and buy can be anything
-        //when ind==n cnt and buy can be anything 
+       vector<int>curr(2*k+1,0);
+        vector<int>ahead(2*k+1,0);
+        // base cases are inititated to zero
         for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-              for (int cnt = 1; cnt <= k; cnt++) {
-                if (buy)
-                  curr[buy][cnt] = max(-price[ind] + after[0][cnt],
-                                          after[1][cnt]);
+            for(int transaction=(2*k)-1;transaction>=0;transaction--){
+                int profit=0;
+                if(transaction%2==0)
+                profit=max(-price[ind]+ahead[transaction+1],ahead[transaction]);
                 else
-                 curr[buy][cnt] = max(price[ind] + after[1][cnt - 1],
-                                          after[0][cnt]);
-              }after=curr;
-            }
-        }return after[1][k];
+                  profit=max(price[ind]+ahead[transaction+1],ahead[transaction]);
+                  curr[transaction]=profit;
+            }ahead=curr;
+        }return ahead[0];
     }
 };
