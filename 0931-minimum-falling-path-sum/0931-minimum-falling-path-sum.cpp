daff4f46e50f1a490232eb975157659ee7a5c6b2
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-05-31 00:22:18 +0530

    Time: 18 ms (41.75%), Space: 10.1 MB (74.99%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n=matrix.size();
+         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,0));
+       vector<int>prev(m,0);
         for(int j=0;j<m;j++)
-            dp[0][j]=matrix[0][j];
+            prev[j]=matrix[0][j];
         
         for(int i=1;i<n;i++){
+            vector<int>curr(m,0);
             for(int j=0;j<m;j++){
-                int up=matrix[i][j]+dp[i-1][j];
+                int up=matrix[i][j]+prev[j];
                 int diag1=0,diag2=0;
                 if(j>=1)
-                    diag1=matrix[i][j]+dp[i-1][j-1];
+                    diag1=matrix[i][j]+prev[j-1];
                 else
-                    diag1=1e9;
+                    diag1=1e8;
                 if(j+1<m)
-                     diag2=matrix[i][j]+dp[i-1][j+1];
+                     diag2=matrix[i][j]+prev[j+1];
                 else
-                    diag2=1e9;
-                dp[i][j]=min(up,min(diag1,diag2));
-            }
+                    diag2=1e8;
+                curr[j]=min(up,min(diag1,diag2));
+            }prev=curr;
         }
-        int mini=1e9;
+        int mini=1e8;
         for(int j=0;j<m;j++){
-            mini=min(mini,dp[n-1][j]);
+            mini=min(mini,prev[j]);
         }return mini;
     }
 };
