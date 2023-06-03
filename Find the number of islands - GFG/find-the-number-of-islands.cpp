ecadb76bbbf2e840ca2dba4bc632c49704997e6d
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-03 20:54:36 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -5,14 +5,11 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
     private:
-   void bfs(vector<vector<char>> &value , vector<vector<int>>&vis,int row,int col)
+   void dfs(vector<vector<char>> &value , vector<vector<int>>&vis,int row,int col)
     {
         vis[row][col]=1;
-        
         int r= value.size();
         int c= value[0].size();
-        
-    
         for(int i=-1;i<=1;i++)
         {
             for(int j=-1;j<=1;j++)
@@ -23,23 +20,24 @@ class Solution {
                if(newRow>=0 and newRow<r and newCol>=0 and newCol<c and
                value[newRow][newCol]=='1' and !vis[newRow][newCol])
                {
-                   bfs(value,vis,newRow,newCol);
+                   dfs(value,vis,newRow,newCol);
                }
                
             }
-        }}
+        }
+    }
   public:
     
     
-    int numIslands(vector<vector<char>>& value) {
-        int n=value.size();
-        int m=value[0].size();
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
         int cnt=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && value[i][j]=='1'){
-                    bfs(value,vis,i,j);
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    dfs(grid,vis,i,j);
                     cnt++;
                 }
             }
