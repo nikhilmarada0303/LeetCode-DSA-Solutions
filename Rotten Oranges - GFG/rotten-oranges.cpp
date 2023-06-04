2d193487d253abde4849ebbce6cdf158d8eaebf3
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-04 13:52:08 +0530

    Added solution - LeetHub

---
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find minimum time required to rot all oranges. 
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int vis[n][m];
+        int cntFresh=0;
+        // {{r,c},time}
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
+                }else
+                    vis[i][j]=0;
+                if(grid[i][j]==1)
+                    cntFresh++;
+            }
+        }int delRow[]={-1,0,1,0};
+        int delCol[]={0,1,0,-1};
+        int tp=0;
+        int cnt=0;
+        while(!q.empty()){
+            int row=q.front().first.first;
+            int col=q.front().first.second;
+            int time=q.front().second;
+            tp=max(tp,time);
+            q.pop();
+            for(int i=0;i<4;i++){
+                int newRow=row+delRow[i];
+                int newCol=col+delCol[i];
+                if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && grid[newRow][newCol]==1 && vis[newRow][newCol]==0){
+                    vis[newRow][newCol]=2;
+                    q.push({{newRow,newCol},time+1});
+                    cnt++;
+                }
+            }
+        }
+        if(cnt!=cntFresh) return -1;
+        return tp;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
