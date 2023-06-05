09b9ce351f081d2477d657610f25f0f0df8f6f8d
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-05 13:16:45 +0530

    Added solution - LeetHub

---
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+    private:
+      bool dfs(int node,vector<int> adj[],int vis[],int path_vis[],int check[]){
+        vis[node]=1;
+        path_vis[node]=1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                if(dfs(it,adj,vis,path_vis,check)==true) return true;
+            }else if(path_vis[it])
+                return true;
+        }
+        check[node]=1;
+        path_vis[node]=0;
+        return false;
+    }
+  public:
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        int check[V]={0};
+         int vis[V]={0};
+        int path_vis[V]={0};
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,adj,vis,path_vis,check);
+            }
+        }vector<int>ans;
+        for(int i=0;i<V;i++){
+            if(check[i]==1)
+                ans.push_back(i);
+        }return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        vector<int> safeNodes = obj.eventualSafeNodes(V, adj);
+        for (auto i : safeNodes) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
