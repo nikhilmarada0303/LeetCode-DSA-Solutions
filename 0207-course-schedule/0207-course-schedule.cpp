fe4c70d35cc3caf8ea0b8401cdb029e63fb5ddf8
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-05 17:28:24 +0530

    Time: 29 ms (32.07%), Space: 13.8 MB (61.27%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& prerequisites) {
+           vector<int>adj[V];
+	   for(auto it:prerequisites){
+	       adj[it[1]].push_back(it[0]);
+	   }
+	    vector<int>in_degree(V,0);
+	    for(int i=0;i<V;i++){
+	        for(auto it:adj[i])
+	            in_degree[it]++;
+	    }
+	    queue<int>q;
+	    vector<int>topo;
+	    for(int i=0;i<V;i++){
+	        if(in_degree[i]==0)
+	            q.push(i);
+	    }
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        topo.push_back(node);
+	        for(auto it:adj[node]){
+	            in_degree[it]--;
+	            if(in_degree[it]==0) 
+	                q.push(it);
+	        }
+	    }
+	    if(V==topo.size())
+	        return true;
+	   return false;
+    }
+};
