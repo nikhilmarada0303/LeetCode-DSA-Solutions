c8c2038c4bcf69ed8fb4b890e5f2c0fa838b76e4
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-06 11:42:10 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -9,17 +9,18 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-    private:
+   /* private:
       bool dfs(int node,vector<int> adj[],int vis[],int path_vis[],int check[]){
         vis[node]=1;
         path_vis[node]=1;
         for(auto it:adj[node]){
             if(!vis[it]){
+                // if there is cycle ,it will return cycle and check will not be marked as 1 and it not a safe node 
                 if(dfs(it,adj,vis,path_vis,check)==true) return true;
             }else if(path_vis[it])
                 return true;
         }
-        check[node]=1;
+        check[node]=1;       // safe node
         path_vis[node]=0;
         return false;
     }
@@ -37,6 +38,35 @@ class Solution {
             if(check[i]==1)
                 ans.push_back(i);
         }return ans;
+    }*/ public:
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        vector<int>adjrev[V];
+        vector<int>indegree(V,0);
+        for(int i=0;i<V;i++){
+            // i--->it     reverse  it---->i
+            for(auto it:adj[i]){
+                adjrev[it].push_back(i);
+                indegree[i]++;
+            }
+        }//topo sort
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0)
+                q.push(i);
+        }
+        vector<int>safe_nodes;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            safe_nodes.push_back(node);
+            for(auto it:adjrev[node]){
+                indegree[it]--;
+                if(indegree[it]==0)
+                    q.push(it);
+            }
+        }
+        sort(safe_nodes.begin(),safe_nodes.end());
+        return safe_nodes;
     }
 };
 
