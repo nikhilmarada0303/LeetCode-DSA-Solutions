636c896471d0907da5496983be096a0dda12ba41
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-09 12:17:34 +0530

    Added solution - LeetHub

---
+++ Path With Minimum Effort - GFG/path-with-minimum-effort.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int MinimumEffort(vector<vector<int>>& heights) {
+        int m=heights.size();
+        int n=heights[0].size();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        pq.push({0,{0,0}});
+        vector<vector<int>>dist(m,vector<int>(n,1e9));
+        dist[0][0]=0;
+        int drow[]={-1,0,1,0}; int dcol[]={0,1,0,-1};
+        while(!pq.empty()){
+            auto it=pq.top();
+            int diff=it.first;
+            int row=it.second.first;
+            int col=it.second.second;
+            pq.pop();
+            if(row==m-1 && col==n-1) return diff;
+
+            for(int i=0;i<4;i++){
+                int n_row=row+drow[i];
+                int n_col=col+dcol[i];
+                if(n_row>=0 && n_row<m && n_col>=0 && n_col<n){
+                    int new_effort=max(diff,abs(heights[row][col]-heights[n_row][n_col]));
+                    if(new_effort<dist[n_row][n_col]){
+                        dist[n_row][n_col]=new_effort;
+                        pq.push({dist[n_row][n_col],{n_row,n_col}});
+                    }
+                }
+            }
+        }return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n,m; cin>>n>>m;
+        vector<vector<int>> heights;
+       
+        for(int i=0; i<n; ++i){
+            vector<int> temp;
+            for(int j=0; j<m; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            heights.push_back(temp);
+        }
+       
+        Solution obj;
+        cout<<obj.MinimumEffort(heights)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
