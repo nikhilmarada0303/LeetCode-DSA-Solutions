c83de11288f18ffbca4005a393703d83ae4e3265
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-10 01:30:05 +0530

    Added solution - LeetHub

--- City With the Smallest Number of Neighbors at a Threshold Distance - GFG/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ City With the Smallest Number of Neighbors at a Threshold Distance - GFG/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -63,7 +63,7 @@ class Solution {
          for(int k=0;k<n;k++){
 	        for(int i=0;i<n;i++){
 	            for(int j=0;j<n;j++){
-	                if(dist[i][k]==1e9 || dist[k][j]==1e9) continue;
+	                
 	                dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
 	            }
 	        }
