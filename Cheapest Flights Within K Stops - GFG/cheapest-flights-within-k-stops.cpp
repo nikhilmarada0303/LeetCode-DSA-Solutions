10090d5caa357c700d92bddc80a540c0ed22a0dc
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-09 14:35:51 +0530

    Added solution - LeetHub

---
+++ Cheapest Flights Within K Stops - GFG/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int CheapestFLight(int n, vector<vector<int>>& flights, int src, int dst, int k)  {
+        /*vector<pair<int,int>>adj[n];
+        for(int i=0;i<flights.size();i++){
+            int u=flights[i][0];
+            int v=flights[i][1];
+            int wt=flights[i][2];
+            adj[u].push_back({v,wt});
+        }
+        vector<int>dist(n,1e9);
+       priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        dist[src]=0;
+        //{dist,{node,stops}}
+        pq.push({0,{src,0}});
+        while(!pq.empty()){
+            int node=pq.top().second.first;
+            int cost=pq.top().first;
+            int stops=pq.top().second.second;
+            pq.pop();
+            
+            if(stops<=K+1 && node==dst) return cost;
+            if(stops>K+1) continue;
+            for(auto it:adj[node]){
+                int adj_node=it.first;
+                int flight_cost=it.second;
+                if(flight_cost+cost<dist[adj_node] && stops<=K){
+                    dist[adj_node]=flight_cost+cost;
+                    pq.push({dist[adj_node],{adj_node,stops+1}});
+                }
+            }
+        }
+        return -1};*/
+         vector<vector<pair<int,int>>> adj(n);
+        for(auto &v:flights)adj[v[0]].push_back({v[1],v[2]});
+        queue<pair<int,pair<int,int>>> q;
+        q.push({0,{src,0}});
+        vector<int> dis(n,1e9);
+        dis[src]=0;
+        while(!q.empty())
+        {
+            auto it=q.front();
+            q.pop();
+            int stops=it.first;
+            int node=it.second.first;
+            int cost=it.second.second;
+            if(stops>k) continue;
+            for(auto &p:adj[node])
+            {
+                int adjNode=p.first;
+                int wt=p.second;
+                if(cost+wt<dis[adjNode])
+                {
+                    dis[adjNode]=cost+wt;
+                    q.push({stops+1,{adjNode,dis[adjNode]}});
+                }
+            }
+        }
+        if(dis[dst]==1e9) return -1;
+        return dis[dst];
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n; cin>>n;
+        int edge; cin>>edge;
+        vector<vector<int>> flights;
+        
+        for(int i=0; i<edge; ++i){
+            vector<int> temp;
+            for(int j=0; j<3; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            flights.push_back(temp);
+        }
+        
+        int src,dst,k;
+        cin>>src>>dst>>k;
+        Solution obj;
+        cout<<obj.CheapestFLight(n,flights,src,dst,k)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
