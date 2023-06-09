900bb0195053096677b49dfb29e7e173461e5e27
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-09 20:01:16 +0530

    Time: 88 ms (79.25%), Space: 30.2 MB (93.98%) - LeetHub

---
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<pair<int,int>>adj[n];
+        for(int i=0;i<roads.size();i++){
+            int u=roads[i][0];
+            int v=roads[i][1];
+            int wt=roads[i][2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
+        }
+        int destination=n-1;
+        //{time,node}
+        priority_queue<pair<long,long>,vector<pair<long,long>>,greater<pair<long,long>>>q;
+        vector<long long>dist(n,1e18);
+        vector<long long>ways(n,0);
+        ways[0]=1;
+        q.push({0,0});
+        dist[0]=0;
+        int mod=(long)(1e9+7);
+        while(!q.empty()){
+            long long time=q.top().first;
+            long long node=q.top().second;
+            q.pop();
+            
+            for(auto it:adj[node]){
+                long n_time=it.second;
+                long adj_node=it.first;
+                //this is the first time i am coming
+                //with the shortest path
+                if(time+n_time<dist[adj_node]){
+                    dist[adj_node]=time+n_time;
+                    ways[adj_node]=ways[node]%mod;
+                    q.push({dist[adj_node],adj_node});
+                }else if(time+n_time==dist[adj_node])
+                    ways[adj_node]=(ways[adj_node]+ways[node])%mod;
+            }
+        }
+        return ways[n-1]%mod;
+    }
+};
