a488b999615befe779eba6344d93308a98e4117c
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-11 17:08:36 +0530

    Time: 83 ms (90.46%), Space: 34.3 MB (46.26%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,64 @@
+class disjointSet{
+    vector<int>size,parent;
+    public:
+        disjointSet(int n){
+            size.resize(n+1,1);
+            parent.resize(n+1);
+            for(int i=0;i<=n;i++)
+                parent[i]=i;
+        }
+        int findParent(int node){
+            if(node==parent[node])
+                return node;
+            return parent[node]=findParent(parent[node]);
+        }
+        void unionBySize(int u,int v){
+            int ult_u=findParent(u);
+            int ult_v=findParent(v);
+	    if(ult_u==ult_v) return;
+            if(size[ult_u]<size[ult_v]){
+                parent[ult_u]=ult_v;
+                size[ult_v]+=size[ult_u];
+            }
+            else{
+                parent[ult_v]=ult_u;
+                size[ult_u]+=size[ult_v];
+            }
+        }
+};
+
+class Solution {
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& details) {
+         int n=details.size();
+        disjointSet ds(n);
+        unordered_map<string,int>mapMailnode;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<details[i].size();j++){
+                string mail=details[i][j];
+                if(mapMailnode.find(mail)==mapMailnode.end())           // not there in map
+                    mapMailnode[mail]=i;
+                else{
+                    ds.unionBySize(i,mapMailnode[mail]);
+                }
+            }
+        }
+        vector<string>mergeMail[n];
+        for(auto it:mapMailnode){
+            int name=ds.findParent(it.second);
+            string mail=it.first;
+            mergeMail[name].push_back(mail);
+        }
+        vector<vector<string>>ans;
+       for(int i=0;i<n;i++){
+            if(mergeMail[i].size()==0) continue;
+            vector<string>temp;
+            sort(mergeMail[i].begin(),mergeMail[i].end());
+            temp.push_back(details[i][0]);
+            for(auto it:mergeMail[i]){
+                temp.push_back(it);
+            }ans.push_back(temp);
+        }sort(ans.begin(), ans.end(), greater<vector<string>>()); 
+        return ans;
+    }
+};
