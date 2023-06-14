637fa5fc6ab73d34f2bf3749230bc0460a4852c2
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-14 08:20:38 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -8,44 +8,47 @@ class Solution
     public:
     //Function to find minimum time required to rot all oranges. 
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int vis[n][m];
-        int cntFresh=0;
-        // {{r,c},time}
-        queue<pair<pair<int,int>,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    vis[i][j]=2;
-                }else
-                    vis[i][j]=0;
+       int m=grid.size();
+       int n=grid[0].size();
+       int vis[m][n];
+       int cntFresh=0;
+       //{time,{r,c}}
+       queue<pair<int,pair<int,int>>>q;
+       for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+               if(grid[i][j]==2){
+                   q.push({0,{i,j}});
+                   vis[i][j]=1;
+               }else
+                vis[i][j]=0;
                 if(grid[i][j]==1)
-                    cntFresh++;
-            }
-        }int delRow[]={-1,0,1,0};
-        int delCol[]={0,1,0,-1};
-        int tp=0;
-        int cnt=0;
-        while(!q.empty()){
-            int row=q.front().first.first;
-            int col=q.front().first.second;
-            int time=q.front().second;
-            tp=max(tp,time);
-            q.pop();
-            for(int i=0;i<4;i++){
-                int newRow=row+delRow[i];
-                int newCol=col+delCol[i];
-                if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && grid[newRow][newCol]==1 && vis[newRow][newCol]==0){
-                    vis[newRow][newCol]=2;
-                    q.push({{newRow,newCol},time+1});
-                    cnt++;
-                }
-            }
-        }
-        if(cnt!=cntFresh) return -1;
-        return tp;
+                    cntFresh++;    //fresh oranges
+           }
+       }
+       int tmin=0;
+       int drow[4]={-1,0,1,0};
+       int dcol[4]={0,1,0,-1};
+       int cnt=0;
+       while(!q.empty()){
+           int time=q.front().first;
+           int row=q.front().second.first;
+           int col=q.front().second.second;
+           q.pop();
+           tmin=time;
+           for(int i=0;i<4;i++){
+               int n_row=row+drow[i];
+               int n_col=col+dcol[i];
+               if(n_row>=0 && n_row<m && n_col>=0 && n_col<n && vis[n_row][n_col]==0 && grid[n_row][n_col]==1){
+                   vis[n_row][n_col]=1;
+                   q.push({time+1,{n_row,n_col}});
+                   cnt++;
+               }
+           }
+           
+       }if(cnt==cntFresh)
+        return tmin;
+        return -1;
+       
     }
 };
 
