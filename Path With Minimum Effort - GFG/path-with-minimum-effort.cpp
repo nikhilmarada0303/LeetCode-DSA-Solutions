541ba1d0343758026a6d624c3773143b9ab87d26
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-15 22:39:48 +0530

    Added solution - LeetHub

--- Path With Minimum Effort - GFG/path-with-minimum-effort.cpp
+++ Path With Minimum Effort - GFG/path-with-minimum-effort.cpp
@@ -13,26 +13,24 @@ class Solution {
         vector<vector<int>>dist(m,vector<int>(n,1e9));
         dist[0][0]=0;
         int drow[]={-1,0,1,0}; int dcol[]={0,1,0,-1};
-        while(!pq.empty()){
-            auto it=pq.top();
-            int diff=it.first;
-            int row=it.second.first;
-            int col=it.second.second;
-            pq.pop();
-            if(row==m-1 && col==n-1) return diff;
-
-            for(int i=0;i<4;i++){
-                int n_row=row+drow[i];
-                int n_col=col+dcol[i];
-                if(n_row>=0 && n_row<m && n_col>=0 && n_col<n){
-                    int new_effort=max(diff,abs(heights[row][col]-heights[n_row][n_col]));
-                    if(new_effort<dist[n_row][n_col]){
-                        dist[n_row][n_col]=new_effort;
-                        pq.push({dist[n_row][n_col],{n_row,n_col}});
-                    }
-                }
-            }
-        }return 0;
+       while(!pq.empty()){
+           int diff=pq.top().first;
+           int row=pq.top().second.first;
+           int col=pq.top().second.second;
+           pq.pop();
+           if(row==m-1 && col==n-1) return diff;
+           for(int i=0;i<4;i++){
+               int n_row=row+drow[i];
+               int n_col=col+dcol[i];
+               if(n_row>=0 && n_col>=0 && n_row<m && n_col<n){
+                   int new_effort=max(diff,abs(heights[n_row][n_col]-heights[row][col]));
+                   if(new_effort<dist[n_row][n_col]){
+                       dist[n_row][n_col]=new_effort;
+                       pq.push({new_effort,{n_row,n_col}});
+                   }
+               }
+           }
+       }return 0;
     }
 };
 
