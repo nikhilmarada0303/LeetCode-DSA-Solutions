9e63104676da310d693335b05b931c61bf61c172
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-15 10:02:20 +0530

    Added solution - LeetHub

--- Word Ladder I - GFG/word-ladder-i.cpp
+++ Word Ladder I - GFG/word-ladder-i.cpp
@@ -6,28 +6,31 @@ using namespace std;
 class Solution {
 public:
     int wordLadderLength(string startWord, string targetWord, vector<string>& wordList) {
-        queue<pair<string,int>>q;
-        q.push({startWord,1});
-        unordered_set<string>st(wordList.begin(),wordList.end());
-        st.erase(startWord);
-        while(!q.empty()){
-            string word=q.front().first;
-            int steps=q.front().second;
-            q.pop();
-            if(word==targetWord)
-                        return steps;
-            for(int i=0;i<word.size();i++){
-                char original=word[i];
-                for(char ch='a';ch<='z';ch++){
-                    word[i]=ch;
-                    if(st.find(word)!=st.end()){
-                        q.push({word,steps+1});
-                        st.erase(word);
-                    }
-                    
-                }word[i]=original;
-            }
-        }return 0;
+       // bfs traversal
+       queue<pair<string,int>>q;
+       unordered_set<string>st;
+       for(int i=0;i<wordList.size();i++){
+           st.insert(wordList[i]);
+       }
+       st.erase(startWord);
+       q.push({startWord,1});
+       while(!q.empty()){
+           int steps=q.front().second;
+           string word=q.front().first;
+           q.pop();
+           if(word==targetWord) return steps;
+           for(int i=0;i<word.size();i++){
+               char original=word[i];
+               for(char ch='a';ch<='z';ch++){
+                   word[i]=ch;
+                   if(st.find(word)!=st.end()) {      // if exists
+                   st.erase(word);
+                    q.push({word,steps+1});
+                   }
+               }word[i]=original;
+           }
+       }
+       return 0;
     }
 };
 
