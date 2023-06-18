41e9f2e5985f1658c6fbabe4e1fd063b2f641b10
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-18 17:08:53 +0530

    Added solution - LeetHub

--- Partition Array for Maximum Sum - GFG/partition-array-for-maximum-sum.cpp
+++ Partition Array for Maximum Sum - GFG/partition-array-for-maximum-sum.cpp
@@ -4,24 +4,21 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-    private:
-    int func(int i,int k,int n,vector<int>&arr,vector<int>&dp){
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int max_ans=INT_MIN;
-        int len=0;
-        int max_element=INT_MIN;
-        for(int j=i;j<min(n,i+k);j++){
-            len++;
-            max_element=max(max_element,arr[j]);
-            int sum=(len*max_element)+func(j+1,k,n,arr,dp);
-            max_ans=max(max_ans,sum);
-        }return dp[i]=max_ans;
-    }
+    // tabul
     public:
     int solve(int n, int k, vector<int>& arr){
-        vector<int>dp(n,-1);
-        return func(0,k,n,arr,dp);
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            int max_ans=INT_MIN;
+            int len=0;
+            int max_element=INT_MIN;
+            for(int j=i;j<min(n,i+k);j++){
+                len++;
+                max_element=max(max_element,arr[j]);
+                int sum=(len*max_element)+dp[j+1];
+                max_ans=max(max_ans,sum);
+            }dp[i]=max_ans;
+        }return dp[0];
     }
 };
 
