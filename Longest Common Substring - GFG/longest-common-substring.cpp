904dfd01ad05ea9a4e90da411ea8ec35f4ec9010
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-06-22 15:55:04 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.cpp
+++ Longest Common Substring - GFG/longest-common-substring.cpp
@@ -5,23 +5,24 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    
-    int longestCommonSubstr (string s1, string s2, int x, int y)
+     int func(int ind1,int ind2,string &s1,string &s2,int &ans,vector<vector<int>>&dp){
+          if(ind1<0 || ind2<0) return 0;
+        int match=0;
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(s1[ind1]==s2[ind2]){
+             match=1+func(ind1-1,ind2-1,s1,s2,ans,dp);
+                 ans=max(ans,match);
+        }
+           int a= func(ind1-1,ind2,s1,s2,ans,dp);
+            int b=func(ind1,ind2-1,s1,s2,ans,dp);
+        return dp[ind1][ind2]=match;
+    }
+    int longestCommonSubstr (string s1, string s2, int n, int m)
     {
-          vector<int>prev(y+1,0),curr(y+1,0);
-          int ans=0;
-
-        for(int ind1=1;ind1<=x;ind1++){
-            for(int ind2=1;ind2<=y;ind2++){
-                 if(s1[ind1-1]==s2[ind2-1]){
-                    curr[ind2]= 1+prev[ind2-1];
-                     ans=max(ans,curr[ind2]);
-                 }
-                else{
-                curr[ind2]=0;}
-                 
-            }prev=curr;
-        }return ans;
+        int ans=0;
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        func(n-1,m-1,s1,s2,ans,dp);
+        return ans;
     }
 };
 
