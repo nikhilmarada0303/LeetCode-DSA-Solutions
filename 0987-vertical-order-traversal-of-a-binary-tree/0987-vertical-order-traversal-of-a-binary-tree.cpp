708ab45f1c850e7b5eb2403d6ff1b0c441ef20bf
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-07-01 16:18:06 +0530

    Time: 8 ms (33.62%), Space: 15.5 MB (5.71%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**  level order traversal
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
@@ -27,6 +28,30 @@ public:
         }
         vector<vector<int>>ans;
         for(auto p:mp){
+            vector<int>col;
+            for(auto q:p.second){
+                col.insert(col.end(),q.second.begin(),q.second.end());
+            }ans.push_back(col);
+        }
+        return ans;
+    }
+};*/
+// inorder traversal
+class Solution {
+    private:
+    void func(TreeNode* node,int x_axis,int y_axis, map<int,map<int,multiset<int>>>&mp){
+        if(node==NULL) return ;
+        mp[x_axis][y_axis].insert(node->val);
+        if(node->left) func(node->left,x_axis-1,y_axis+1,mp);
+        if(node->right) func(node->right,x_axis+1,y_axis+1,mp);
+        
+    }
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>>ans;
+         map<int,map<int,multiset<int>>>mp;
+        func(root,0,0,mp);
+         for(auto p:mp){
             vector<int>col;
             for(auto q:p.second){
                 col.insert(col.end(),q.second.begin(),q.second.end());
