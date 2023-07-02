80194ca00140b26f5a4a291b9defe1038f19e9e2
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-07-02 11:51:51 +0530

    Time: 11 ms (5.31%), Space: 12.1 MB (19.37%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
 class Solution {
     private:
     void func(TreeNode* node,int level,vector<int>&ans){
@@ -22,5 +23,27 @@ public:
         vector<int>ans;
         func(root,0,ans);
         return ans;
+    }
+};*/
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<int>ans;
+        if(root==NULL)
+            return ans;
+        while(!q.empty()){
+          int size=q.size();
+            int a=0;
+            for(int i=0;i<size;i++){
+                TreeNode* node=q.front();
+                q.pop();
+                a=node->val;
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            ans.push_back(a);
+        }return ans;
     }
 };
