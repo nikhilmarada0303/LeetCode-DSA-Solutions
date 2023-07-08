26620ae132d5f3ac4e7ebfb5162bbea73527e1b1
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-07-09 01:10:49 +0530

    Time: 8 ms (81.25%), Space: 8.1 MB (68.26%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,8 +12,7 @@ public:
         if(head==NULL || head->next==NULL) return false;
         ListNode* fast=head;
         ListNode* slow=head;
-        while(fast->next && fast->next->next){
-           
+        while(fast->next && fast->next->next){  
             fast=fast->next->next;
             slow=slow->next;
              if(fast==slow) return true;
