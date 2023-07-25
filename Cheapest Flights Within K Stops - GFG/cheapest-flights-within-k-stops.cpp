e4b017bbaf04f6ae621a42f3cfe50149efc43ae3
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-07-25 15:26:11 +0530

    Added solution - LeetHub

--- Cheapest Flights Within K Stops - GFG/cheapest-flights-within-k-stops.cpp
+++ Cheapest Flights Within K Stops - GFG/cheapest-flights-within-k-stops.cpp
@@ -6,36 +6,6 @@ using namespace std;
 class Solution {
   public:
     int CheapestFLight(int n, vector<vector<int>>& flights, int src, int dst, int K)  {
-        /*vector<pair<int,int>>adj[n];
-        for(int i=0;i<flights.size();i++){
-            int u=flights[i][0];
-            int v=flights[i][1];
-            int wt=flights[i][2];
-            adj[u].push_back({v,wt});
-        }
-        vector<int>dist(n,1e9);
-       priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        dist[src]=0;
-        //{dist,{node,stops}}
-        pq.push({0,{src,0}});
-        while(!pq.empty()){
-            int node=pq.top().second.first;
-            int cost=pq.top().first;
-            int stops=pq.top().second.second;
-            pq.pop();
-            
-            if(stops<=K+1 && node==dst) return cost;
-            if(stops>K+1) continue;
-            for(auto it:adj[node]){
-                int adj_node=it.first;
-                int flight_cost=it.second;
-                if(flight_cost+cost<dist[adj_node]){
-                    dist[adj_node]=flight_cost+cost;
-                    pq.push({dist[adj_node],{adj_node,stops+1}});
-                }
-            }
-        }
-        return -1;}*/
         vector<pair<int,int>>adj[n];
         for(int i=0;i<flights.size();i++){
             int u=flights[i][0];
@@ -58,7 +28,7 @@ class Solution {
             for(auto it:adj[node]){
                 int adj_node=it.first;
                 int flight_cost=it.second;
-                if(flight_cost+cost<dist[adj_node] && stops<=K){
+                if(flight_cost+cost<dist[adj_node]){
                     dist[adj_node]=flight_cost+cost;
                     q.push({stops+1,{adj_node,dist[adj_node]}});
                 }
@@ -66,8 +36,7 @@ class Solution {
         }
         if(dist[dst]==1e9) return -1;
         return dist[dst];
-        }
-    
+    }
 };
 
 //{ Driver Code Starts.
