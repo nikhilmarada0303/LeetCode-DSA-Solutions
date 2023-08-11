6c4058ad19b9c657c93053e2e73ea2068cb71b14
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-08-11 19:31:12 +0530

    Time: 38 ms (44.07%), Space: 18.2 MB (65.51%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,15 +2,29 @@ class Solution {
 public:
     int func(int ind,int c,vector<int>& coins,vector<vector<int>>&dp){
         if(ind==0) return c%coins[ind]==0;
-        int pick=0;
         if(dp[ind][c]!=-1) return dp[ind][c];
+        int pick=0;
         if(c>=coins[ind])
         pick=func(ind,c-coins[ind],coins,dp);
         int not_pick=func(ind-1,c,coins,dp);
         return dp[ind][c]=pick+not_pick;
     }
     int change(int sum, vector<int>& coins) {
-        vector<vector<int>>dp(coins.size(),vector<int>(sum+1,-1));
-       return func(coins.size()-1,sum,coins,dp);
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(sum+1,0));
+        
+        for(int i=0;i<=sum;i++){
+            dp[0][i]=(i%coins[0]==0);
+        }
+        
+        for(int ind=1;ind<n;ind++){
+            for(int c=0;c<=sum;c++){
+                int pick=0;
+                if(c>=coins[ind])
+                pick=dp[ind][c-coins[ind]];
+                int not_pick=dp[ind-1][c];
+                 dp[ind][c]=pick+not_pick;
+            }
+        }return dp[n-1][sum];
     }
 };
