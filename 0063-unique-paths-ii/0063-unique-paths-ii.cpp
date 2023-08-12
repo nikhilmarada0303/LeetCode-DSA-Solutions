8938ec3b602af09789c5c235b0c308bd1958cdac
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-08-12 16:53:31 +0530

    Time: 5 ms (28.03%), Space: 7.9 MB (6.12%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -14,7 +14,23 @@ public:
         int n=grid.size();
         int m=grid[0].size();
         if(grid[n-1][m-1]==1 || grid[0][0]==1) return 0;
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return func(grid.size()-1,grid[0].size()-1,grid,dp);
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        // return func(grid.size()-1,grid[0].size()-1,grid,dp);
+        
+        dp[0][0]=1;
+                int mod=(int)1e9+7;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) dp[i][j]=0;
+                else if(i==0 && j==0) dp[i][j]=1;
+                else{
+                int left=0,right=0;
+                if(i>=1)
+                left=dp[i-1][j];
+                if(j>=1)
+                right=dp[i][j-1];
+                dp[i][j]=(left+right);}
+            }
+        }return dp[n-1][m-1];
     }
 };
