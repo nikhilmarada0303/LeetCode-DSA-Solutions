0d6e24f3af5690eb817c85501d4a757ad652ca94
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-08-19 00:07:18 +0530

    Time: 634 ms (51.11%), Space: 130.4 MB (37.57%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -3,45 +3,38 @@ public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
         
-        vector<vector<pair<int, int>>> adj(n);  // Adjacency list
-        
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-                adj[i].emplace_back(j, dist);
-                adj[j].emplace_back(i, dist);
+        vector<pair<int,int>>adj[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                    if(i!=j){
+                    int dist=abs(points[i][0]-points[j][0])+abs(points[j][1]-points[i][1]);
+                    adj[i].push_back({j,dist});
+                     // adj[j].push_back({i,dist});
+                    } 
+                
             }
         }
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> > pq;
+        pq.push({0,0,});
+        vector<int>vis(n,0);
+        int sum=0;
         
-        vector<int> minDist(n, INT_MAX);  // Stores the minimum distance to each point
-        vector<bool> vis(n, false);       // Marks visited points
-        
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;  // Min-heap
-        pq.push({0, 0});  // Initial point to the priority queue
-        int totalCost = 0;
-        
-        while (!pq.empty()) {
-            int curDist = pq.top().first;
-            int curNode = pq.top().second;
+        while(!pq.empty()){
+            int wt=pq.top().first;
+            int node=pq.top().second;
             pq.pop();
             
-            if (vis[curNode]) {
-                continue;
-            }
-            vis[curNode] = true;
-            totalCost += curDist;
+            if(vis[node]==1) continue;
+            vis[node]=1;
+            sum+=wt;
             
-            // Update minimum distances for neighboring points using adjacency list
-            for (const auto& neighbor : adj[curNode]) {
-                int nextNode = neighbor.first;
-                int newDist = neighbor.second;
-                if (!vis[nextNode] && newDist < minDist[nextNode]) {
-                    minDist[nextNode] = newDist;
-                    pq.push({newDist, nextNode});
+            for(auto it:adj[node]){
+                int n_wt=it.second;
+                int n_node=it.first;
+                if(!vis[n_node]){
+                    pq.push({n_wt,n_node});
                 }
             }
-        }
-        
-        return totalCost;
+        }return sum;
     }
 };
