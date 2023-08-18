6b71fdb84915cdd50d9600e4285a100f70220efd
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-08-18 23:59:12 +0530

    Time: 118 ms (92.29%), Space: 102.1 MB (46.62%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,86 +1,47 @@
-// class Solution {
-// public:
-//     int minCostConnectPoints(vector<vector<int>>& edges) {
-//         int n=edges.size();
-        
-//         vector<pair<int,int>>adj[n];
-//         for(auto it:edges){
-//             int node=it[0];
-            
-//             for(auto i:edges){
-//                 if(i[0]!=node){
-//                     int dist=abs(i[0]-it[0])+abs(it[1]-i[1]);
-//                     adj[it[0]].push_back({i[0],dist});
-//                      adj[i[0]].push_back({it[0],dist});
-                    
-//                 }
-//             }
-//         }
-//         vector<int>vis(n,0);
-// priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-//         pq.push({0,{0,-1}});
-        
-//         int sum=0;
-//         while(!pq.empty()){
-//             int wt=pq.top().first;
-//             int node=pq.top().second.first;
-//             int parent=pq.top().second.second;
-//             pq.pop();
-            
-//             if(vis[node]==1) continue;
-//             vis[node]=1;
-//             sum+=wt;
-            
-//             for(auto it:adj[node]){
-//                 int n_wt=it.second;
-//                 int n_node=it.first;
-//                 int n_parent=node;
-//                 if(!vis[n_node])
-//                     pq.push({n_wt,{n_node,n_parent}});
-//             }
-            
-//             }return sum;
-//     }
-// };
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
         
-        vector<vector<int>> adj(n, vector<int>(n, 0));
+        vector<vector<pair<int, int>>> adj(n);  // Adjacency list
+        
         for (int i = 0; i < n; ++i) {
             for (int j = i + 1; j < n; ++j) {
                 int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-                adj[i][j] = dist;
-                adj[j][i] = dist;
+                adj[i].emplace_back(j, dist);
+                adj[j].emplace_back(i, dist);
             }
         }
         
-        vector<int> vis(n, 0);
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        pq.push({0, {0, -1}});
+        vector<int> minDist(n, INT_MAX);  // Stores the minimum distance to each point
+        vector<bool> vis(n, false);       // Marks visited points
         
-        int sum = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;  // Min-heap
+        pq.push({0, 0});  // Initial point to the priority queue
+        int totalCost = 0;
+        
         while (!pq.empty()) {
-            int wt = pq.top().first;
-            int node = pq.top().second.first;
-            int parent = pq.top().second.second;
+            int curDist = pq.top().first;
+            int curNode = pq.top().second;
             pq.pop();
             
-            if (vis[node] == 1) continue;
-            vis[node] = 1;
-            sum += wt;
+            if (vis[curNode]) {
+                continue;
+            }
+            vis[curNode] = true;
+            totalCost += curDist;
             
-            for (int i = 0; i < n; ++i) {
-                if (i != node && !vis[i]) {
-                    int n_wt = adj[node][i];
-                    int n_node = i;
-                    int n_parent = node;
-                    pq.push({n_wt, {n_node, n_parent}});
+            // Update minimum distances for neighboring points using adjacency list
+            for (const auto& neighbor : adj[curNode]) {
+                int nextNode = neighbor.first;
+                int newDist = neighbor.second;
+                if (!vis[nextNode] && newDist < minDist[nextNode]) {
+                    minDist[nextNode] = newDist;
+                    pq.push({newDist, nextNode});
                 }
             }
         }
         
-        return sum;
+        return totalCost;
     }
 };
