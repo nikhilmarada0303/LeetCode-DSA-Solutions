3a89daab3b095c25537194d47cb253e9cc7c1b1f
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2023-08-19 22:24:55 +0530

    Time: 904 ms (22.54%), Space: 292.5 MB (8.57%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,35 +1,21 @@
-/*class Solution {
-    private:
-    int func(int ind,int prev_ind,vector<int>&nums,int n,vector<vector<int>>&dp){
-        if(ind==n) return 0;
-        if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
-        int take=0;
-        if(prev_ind==-1 || nums[ind]>nums[prev_ind])
-            take=1+func(ind+1,ind,nums,n,dp);
-        int nottake=func(ind+1,prev_ind,nums,n,dp);
-        return dp[ind][prev_ind+1]=max(take,nottake);
-    }
+class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    int func(int ind,int prev_ind,vector<int>&nums,int n,vector<vector<int>>&dp){
+        if(ind==n){
+            // if(nums[ind-1]<nums[ind])
+            // return 1;
+            // return 0;
+            return 0;
+        }if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
+        int take=0;
+        if(prev_ind==-1 || nums[prev_ind]<nums[ind])
+           take=1+func(ind+1,ind,nums,n,dp);
+        int not_take=func(ind+1,prev_ind,nums,n,dp);
+        return dp[ind][prev_ind+1]=max(take,not_take);
+    }
+    int lengthOfLIS(vector<int>&nums) {
         int n=nums.size();
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         return func(0,-1,nums,n,dp);
-    }
-};
-*/
-
-
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& a) {
-        int n=a.size();
-         vector<int>dp(n,1);
-       int maxi=1;
-       for(int i=0;i<n;i++){
-           for(int prev=0;prev<i;prev++){
-               if(a[prev]<a[i])
-                dp[i]=max(dp[i],1+dp[prev]);
-           }maxi=max(maxi,dp[i]);
-       }return maxi;
     }
 };
