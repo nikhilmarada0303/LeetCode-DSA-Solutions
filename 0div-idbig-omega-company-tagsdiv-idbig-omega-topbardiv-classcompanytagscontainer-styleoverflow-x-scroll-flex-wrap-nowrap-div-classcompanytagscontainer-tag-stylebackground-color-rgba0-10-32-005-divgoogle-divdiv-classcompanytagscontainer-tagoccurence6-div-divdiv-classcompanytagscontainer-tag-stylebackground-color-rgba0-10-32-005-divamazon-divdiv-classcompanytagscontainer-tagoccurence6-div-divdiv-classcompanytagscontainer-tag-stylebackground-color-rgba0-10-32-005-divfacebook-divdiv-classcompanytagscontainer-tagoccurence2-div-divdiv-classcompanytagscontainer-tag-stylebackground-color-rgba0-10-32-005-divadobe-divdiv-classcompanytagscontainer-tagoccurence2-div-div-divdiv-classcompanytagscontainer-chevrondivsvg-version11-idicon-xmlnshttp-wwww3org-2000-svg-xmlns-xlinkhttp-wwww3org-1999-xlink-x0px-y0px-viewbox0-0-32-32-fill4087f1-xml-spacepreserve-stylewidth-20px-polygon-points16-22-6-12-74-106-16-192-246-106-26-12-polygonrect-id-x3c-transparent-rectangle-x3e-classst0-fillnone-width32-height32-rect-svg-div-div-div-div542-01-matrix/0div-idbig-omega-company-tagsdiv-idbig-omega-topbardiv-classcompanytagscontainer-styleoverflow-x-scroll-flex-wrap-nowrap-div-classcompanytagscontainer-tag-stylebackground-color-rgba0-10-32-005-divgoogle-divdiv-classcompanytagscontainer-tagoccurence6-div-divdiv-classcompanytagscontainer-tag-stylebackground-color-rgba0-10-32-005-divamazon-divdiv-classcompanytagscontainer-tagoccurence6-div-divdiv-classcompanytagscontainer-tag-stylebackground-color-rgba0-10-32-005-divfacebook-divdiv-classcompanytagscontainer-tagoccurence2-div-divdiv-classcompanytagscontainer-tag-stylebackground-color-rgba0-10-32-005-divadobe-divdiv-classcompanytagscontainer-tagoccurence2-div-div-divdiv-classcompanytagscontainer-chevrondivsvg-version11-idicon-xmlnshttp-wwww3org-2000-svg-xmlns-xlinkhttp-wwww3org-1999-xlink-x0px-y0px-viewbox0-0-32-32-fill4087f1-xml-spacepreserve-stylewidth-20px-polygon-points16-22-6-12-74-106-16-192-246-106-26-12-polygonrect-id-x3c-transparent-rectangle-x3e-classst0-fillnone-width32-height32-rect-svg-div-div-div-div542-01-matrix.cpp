b6715d49e5c8df3b5b430a9d87e98351d7d38b41
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2024-01-20 12:22:08 +0530

    Time: 47 ms (97.98%), Space: 37.8 MB (16.86%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div542-01-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div542-01-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+       vector<vector<int>>vis(n,vector<int>(m,0));
+         vector<vector<int>>dist(n,vector<int>(m,0));
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         if(mat[i][j]==0)
+        //             vis[i][j]=1;
+        //     }
+        // }
+        queue<pair<int,pair<int,int>>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.push({0,{i,j}});
+                    vis[i][j]=1;}
+            }
+        }
+        while(!q.empty()){
+            int row=q.front().second.first;
+            int col=q.front().second.second;
+            int dis=q.front().first;
+            q.pop();
+            dist[row][col]=dis;
+            
+            int drow[4]={-1,0,1,0};
+            int dcol[4]={0,1,0,-1};
+            for(int i=0;i<4;i++){
+                int n_row=row+drow[i];
+                int n_col=col+dcol[i];
+                if(n_row>=0 && n_row<n && n_col>=0 && n_col<m && vis[n_row][n_col]==0 ){
+                    vis[n_row][n_col]=1;
+                    q.push({dis+1,{n_row,n_col}});
+                }
+            }
+        }return dist;
+    }
+};
