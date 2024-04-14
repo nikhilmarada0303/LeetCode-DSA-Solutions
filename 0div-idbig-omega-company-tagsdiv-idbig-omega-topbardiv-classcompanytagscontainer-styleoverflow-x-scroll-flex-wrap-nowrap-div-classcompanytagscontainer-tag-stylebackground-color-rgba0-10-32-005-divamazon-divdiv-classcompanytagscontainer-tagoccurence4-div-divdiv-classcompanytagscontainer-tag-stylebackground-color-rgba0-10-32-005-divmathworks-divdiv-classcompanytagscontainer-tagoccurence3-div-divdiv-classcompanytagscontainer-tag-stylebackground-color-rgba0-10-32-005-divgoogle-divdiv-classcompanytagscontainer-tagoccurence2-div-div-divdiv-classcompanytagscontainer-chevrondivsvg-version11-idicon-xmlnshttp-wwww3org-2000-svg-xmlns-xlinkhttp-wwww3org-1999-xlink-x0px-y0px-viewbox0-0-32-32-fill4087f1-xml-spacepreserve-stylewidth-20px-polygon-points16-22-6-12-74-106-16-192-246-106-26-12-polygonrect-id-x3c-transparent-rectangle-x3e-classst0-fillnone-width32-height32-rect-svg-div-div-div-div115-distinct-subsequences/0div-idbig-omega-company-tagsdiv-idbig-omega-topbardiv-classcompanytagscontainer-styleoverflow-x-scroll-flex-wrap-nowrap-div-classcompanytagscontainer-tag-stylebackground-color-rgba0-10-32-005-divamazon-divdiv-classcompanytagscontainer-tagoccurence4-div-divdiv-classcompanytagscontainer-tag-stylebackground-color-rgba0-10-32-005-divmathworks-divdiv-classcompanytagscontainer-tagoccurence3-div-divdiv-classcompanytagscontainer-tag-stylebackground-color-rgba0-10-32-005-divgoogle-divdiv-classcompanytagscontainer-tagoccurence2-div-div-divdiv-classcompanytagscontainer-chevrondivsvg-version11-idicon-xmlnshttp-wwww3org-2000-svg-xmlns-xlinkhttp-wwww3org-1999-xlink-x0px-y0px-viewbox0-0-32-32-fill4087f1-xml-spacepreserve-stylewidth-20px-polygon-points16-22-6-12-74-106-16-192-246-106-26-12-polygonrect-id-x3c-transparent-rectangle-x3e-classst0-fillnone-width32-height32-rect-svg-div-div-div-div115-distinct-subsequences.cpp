e5be38b293cc09312f1db8ff7f2fa67dfe4657c1
nikhilmarada0303 <105623800+nikhilmarada0303@users.noreply.github.com>
2024-04-15 00:14:14 +0530

    Time: 1708 ms (13.30%), Space: 421.6 MB (6.84%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmathworks-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div115-distinct-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmathworks-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div115-distinct-subsequences.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+//     int func(int ind1,int ind2,string s,string t,vector<vector<int>>&dp){
+//         if(ind2<0) return 1;
+//         if(ind1<0) return 0;
+        
+//         if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+//         if(s[ind1]==t[ind2]){
+//             return dp[ind1][ind2]=func(ind1-1,ind2-1,s,t,dp)+func(ind1-1,ind2,s,t,dp);
+//         }
+//         return dp[ind1][ind2]=func(ind1-1,ind2,s,t,dp);
+        
+//     }
+//     int numDistinct(string s, string t) {
+//         int n=s.size();
+//         int m=t.size();
+//         vector<double>prev(m+1,0);
+//         prev[0]=1;
+        
+//         for(int ind1=1;ind1<=n;ind1++){
+//             for(int ind2=m;ind2>=1;ind2--){
+//                 if(s[ind1-1]==t[ind2-1]){
+//                 prev[ind2]=prev[ind2-1]+prev[ind2];
+//         }
+           
+//             };
+//         }return (int)prev[m];
+//     }
+     int func(int ind1,int ind2,string s,string t, vector<vector<int>>&dp){
+        if(ind2<0) return 1;
+        if(ind1<0) return 0;
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(s[ind1]==t[ind2])
+            return dp[ind1][ind2]=func(ind1-1,ind2-1,s,t,dp)+func(ind1-1,ind2,s,t,dp);
+        return dp[ind1][ind2]=func(ind1-1,ind2,s,t,dp);
+    }
+public:
+    int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return func(n-1,m-1,s,t,dp);
+    }
+
+};
